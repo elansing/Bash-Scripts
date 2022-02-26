d302f7009d8509a0f86f43225fe0c1811ed7c054
Etiénne Lansing <Development.Github@elansing.de>
2022-02-26 20:59:12 +0100

    changed eMail Adress

--- Monitoring-Restart.sh
+++ Monitoring-Restart.sh
@@ -8,14 +8,14 @@ if [[ "$DockerStop" = "$CTID" ]]; then
     DockerStart=`docker start e98ce160983c`
     Datum=`date +%d.%m.%Y`
     Zeit=`date +%H:%M`
-    echo "Erfolgreicher Monitor Restart am $Datum um $Zeit Uhr" | mail -s "Erfolgreich: Monitor Restart" etienne@elansing.de
+    echo "Erfolgreicher Monitor Restart am $Datum um $Zeit Uhr" | mail -s "Erfolgreich: Monitor Restart" ZapHosting-VPS1@elansing.de
   else
     Datum=`date +%d.%m.%Y`
     Zeit=`date +%H:%M`
-    echo "Fehlgeschlagener Monitor Restart am $Datum um $Zeit Uhr" | mail -s "Fehlgeschlagen: Monitor Restart" etienne@elansing.de
+    echo "Fehlgeschlagener Monitor Restart am $Datum um $Zeit Uhr" | mail -s "Fehlgeschlagen: Monitor Restart" ZapHosting-VPS1@elansing.de
   fi
 else
   Datum=`date +%d.%m.%Y`
   Zeit=`date +%H:%M`
-  echo "Fehlgeschlagener Monitor Restart am $Datum um $Zeit Uhr" | mail -s "Fehlgeschlagen: Monitor Restart" etienne@elansing.de
+  echo "Fehlgeschlagener Monitor Restart am $Datum um $Zeit Uhr" | mail -s "Fehlgeschlagen: Monitor Restart" ZapHosting-VPS1@elansing.de
 fi
