a4b4b631540db7c82280869028812bed69b59726
Etiénne Lansing <Development.Github@elansing.de>
2022-06-05 19:05:02 +0200

    changed CTID to Monitoring

--- Monitoring-Restart.sh
+++ Monitoring-Restart.sh
@@ -1,11 +1,11 @@
-CTID="e98ce160983c"
+CTID="Monitoring"
 
-DockerStop=`docker stop e98ce160983c`
+DockerStop=`docker stop Monitoring`
 if [[ "$DockerStop" = "$CTID" ]]; then
   sleep 10
-  DockerStart=`docker start e98ce160983c`
+  DockerStart=`docker start Monitoring`
   if [[ "$DockerStart" = "$CTID" ]]; then
-    DockerStart=`docker start e98ce160983c`
+    DockerStart=`docker start Monitoring`
     Datum=`date +%d.%m.%Y`
     Zeit=`date +%H:%M`
     echo "Erfolgreicher Monitor Restart am $Datum um $Zeit Uhr" | mail -s "Erfolgreich: Monitor Restart" ZapHosting-VPS1@elansing.de
