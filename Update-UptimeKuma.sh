ef828c5b31ca7c0efa557b9b7ec28be43d245d44
Etiénne Lansing <Development.Github@elansing.de>
2022-06-05 19:44:34 +0200

    rm Script

--- Update-UptimeKuma.sh
+++ Update-UptimeKuma.sh
@@ -1,3 +1,4 @@
 docker stop Monitoring
 docker rm Monitoring
 docker run -d --restart=always -p 82:3001 -v /home/Docker/Monitoring:/app/data --name Monitoring louislam/uptime-kuma:latest
+rm Update-UptimeKuma.sh
