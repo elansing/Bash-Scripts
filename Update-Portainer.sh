387e5887300689e221694c49a1f9027df50d6258
Etiénne Lansing <Development.Github@elansing.de>
2022-06-05 19:57:17 +0200

    rm Script

--- Update-Portainer.sh
+++ Update-Portainer.sh
@@ -5,3 +5,4 @@ docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
     portainer/portainer-ce:latest
+rm Update-Portainer.sh
