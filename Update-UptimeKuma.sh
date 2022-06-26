80159c7f8f6c4805fc6af59db80340251a00bdb2
Etiénne Lansing <Development.Github@elansing.de>
2022-06-26 17:06:29 +0200

    added Docker Pull

--- Update-UptimeKuma.sh
+++ Update-UptimeKuma.sh
@@ -1,4 +1,5 @@
 docker stop Monitoring
 docker rm Monitoring
+docker pull louislam/uptime-kuma
 docker run -d --restart=always -p 82:3001 -v /home/Docker/Monitoring:/app/data --name Monitoring louislam/uptime-kuma:latest
 rm Update-UptimeKuma.sh
