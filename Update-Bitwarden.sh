fc143c3f073e081af2fae3b2dd781adae2ea3daf
Etiénne Lansing <Development.Github@elansing.de>
2022-12-25 09:37:17 +0100

    added Update Bitwarden

---
+++ Update-Bitwarden.sh
@@ -0,0 +1,4 @@
+docker stop Bitwarden
+docker rm Bitwarden
+docker pull vaultwarden/server:latest
+docker run -d --name Bitwarden -v /home/Docker/Bitwarden/:/data/ -p 81:80 -p 3012:3012 vaultwarden/server:latest
