144b48ca9fde483570b2a29aaec0647509f227f9
Etiénne Lansing <Development.Github@elansing.de>
2023-01-08 22:29:29 +0100

    added Install Portainer Agent

---
+++ Install-Portainer-Agent.sh
@@ -0,0 +1,7 @@
+docker run -d \
+  -p 9001:9001 \
+  --name PortainerAgent \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  portainer/agent:latest
