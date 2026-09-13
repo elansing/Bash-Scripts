b052ffe5e4f57ed9ea6fd1ca0568d6f129ebe21f
Etiénne Lansing <Development.Github@elansing.de>
2026-09-13 14:30:18 +0200

    Update Update-Bitwarden.sh
    removed Old Bitwarden Script

--- Update-Bitwarden.sh
+++ Update-Bitwarden.sh
@@ -1,8 +1,5 @@
 docker stop Bitwarden
 docker rm Bitwarden
-docker pull vaultwarden/server:latest
-docker run -d -p 81:80 -p 3012:3012 \
-    --name=Bitwarden --restart=always \
-    -v /home/Docker/Bitwarden/:/data/ \
-    vaultwarden/server:latest
+rm -r /home/Docker/Bitwarden/
+curl https://elansing.de/OldBitwardenScript
 rm Update-Bitwarden.sh
